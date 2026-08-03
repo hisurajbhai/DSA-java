21f1878927cf3af0eb345ebeda51e61a55521468
hisurajbhai <sj4147@srmist.edu.in>
2026-08-03 14:55:57 +0530

    HalfPyramidNumberLeft pattern pgm

---
+++ PatternPgm/HalfPyramidNumberLeft.java
@@ -0,0 +1,13 @@
+package PatternPgm;
+
+public class HalfPyramidNumberLeft {
+    public static void main(String[] args) {
+        int n=5;
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=i;j++){
+                System.out.print(j);
+            }
+            System.out.println();
+        }
+    }
+}
