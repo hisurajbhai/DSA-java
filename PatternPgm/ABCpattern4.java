22806f3d46a9f1e2201143830b2d7af92263157c
hisurajbhai <sj4147@srmist.edu.in>
2026-08-04 16:17:54 +0530

    ABCpattern4 pgm

---
+++ PatternPgm/ABCpattern4.java
@@ -0,0 +1,13 @@
+package PatternPgm;
+
+public class ABCpattern4{
+    public static void main(String[] args) {
+        for(int i= 1;i<=5;i++){
+            for(int j=69;j>69-i;j--){
+                char c=(char)j;
+                System.out.print(c+" ");
+            }
+            System.out.println();
+        }
+    }
+}
